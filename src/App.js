f0d4d235c3c708a97c49c48ca04d45e9982bca55
DanujaV <danujagreru@gmail.com>
2021-08-23 21:46:55 +0530

    get the token and find the user

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log('person', user);
+      })
+    }
+
+
+
+    console.log("I HAVE A TOKEN>>> ", token)
+  }, []);
+
   return (
     <div className="app">
       
-
-        {/* spotify Logo */}
-        {/* Login with spotify Button */}
-        <Login />
+      {
+          token ? <Player /> : <Login />
+      }
     </div>
   );
 }
