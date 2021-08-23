bd81ee6a4c418413cd9a853dd8f0fe26a2ec1609
DanujaV <danujagreru@gmail.com>
2021-08-23 20:58:14 +0530

    create auth (login URL)  for spotify login

---
+++ src/spotify.js
@@ -0,0 +1,18 @@
+//https://developer.spotify.com/
+//documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize/";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "1022823106e34dc683b469e39b22626c";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
