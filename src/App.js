14dcb9302ce3cb094dd661514bf2c3e769eb6183
DanujaV <danujagreru@gmail.com>
2021-08-24 20:17:04 +0530

    done with side-bar and load my playlist for side bar

--- src/App.js
+++ src/App.js
@@ -33,6 +33,12 @@ function App() {
           user: user
         })
       });
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        })
+      })
     }
 
   }, []);
--- src/Player.js
+++ src/Player.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Player.css';
+import Sidebar from './Sidebar';
+import Body from './Body';
+import Footer from './Footer';
 
 function Player() {
     return (
-        <div>
-            <h1>Player componet is here</h1>
+        <div className="player">
+            <div className="player__body">
+                <Sidebar />
+                <Body />
+            </div>
+
+        
+            <Footer />
         </div>
     )
 }
