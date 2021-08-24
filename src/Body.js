5145658586d14cea7236c8827b21b43b0ea4b364
DanujaV <danujagreru@gmail.com>
2021-08-24 22:00:00 +0530

    complete body section's Header component

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Body.css';
+import Header from './Header';
 
-function Body() {
+function Body({ spotify }) {
     return (
         <div className="body">
-            <h1>I am the Body</h1>
+            <Header spotify={spotify} />
+
+            
         </div>
     )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    token: "BQDHXRavhA1i9DAiERj77VT8cRPB0cqZuMN-zgXlXfUSnCXXdqyxUPPjdFwCafd2y21s-JVqvgZ223koKGid6SweeL3zCT5rh0s---vRJj102tRmBvRymVahmYMxtIqVhIP7arSA-sLtr2ZEoCRTMfXrW_TL3Hi-S9S2JzJcJ2WawvyLkCxr",
+    // token: "BQDHXRavhA1i9DAiERj77VT8cRPB0cqZuMN-zgXlXfUSnCXXdqyxUPPjdFwCafd2y21s-JVqvgZ223koKGid6SweeL3zCT5rh0s---vRJj102tRmBvRymVahmYMxtIqVhIP7arSA-sLtr2ZEoCRTMfXrW_TL3Hi-S9S2JzJcJ2WawvyLkCxr",
 };
 
 const reducer = (state, action) => {
